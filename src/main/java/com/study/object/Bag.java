08efb75c22f8bc5e844acf4cc6ec3d00724e3395
오세진 <osejin@osejin-ui-Macmini.local>
2022-09-29 21:29:38 +0900

    더 개선, 하지만 의존성이 추가됨. 트레이드오프의 문제

--- src/main/java/com/study/object/Bag.java
+++ src/main/java/com/study/object/Bag.java
@@ -6,28 +6,31 @@ public class Bag {
     private Invitation invitation;
     private Ticket ticket;
 
-    public Bag(Long amount) {
-        this.amount = amount;
+    public Long hold(Ticket ticket) {
+        if (hasInvitation()) {
+            setTicket(ticket);
+
+            return 0L;
+        } else {
+            setTicket(ticket);
+            minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
     }
 
-    public Bag(Long amount, Invitation invitation) {
-        this.amount = amount;
-        this.invitation = invitation;
-    }
-
-    public boolean hasInvitation() {
+    private boolean hasInvitation() {
         return invitation != null;
     }
 
-    public boolean hasTicket() {
+    private boolean hasTicket() {
         return ticket != null;
     }
 
-    public void setTicket(Ticket ticket) {
+    private void setTicket(Ticket ticket) {
         this.ticket = ticket;
     }
 
-    public void minusAmount(Long amount) {
+    private void minusAmount(Long amount) {
         this.amount -= amount;
     }
 
